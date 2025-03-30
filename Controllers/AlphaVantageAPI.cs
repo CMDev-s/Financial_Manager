19ce08fe172722172beacee098aaf272fbc7941f
Camila Schmith <schmithcamila@gmail.com>
2025-03-30 18:12:01 -0300

    Primeira estrutura Conexão API

---
+++ Controllers/AlphaVantageAPI.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace WebFinancial.Controllers
+{
+    public class AlphaVantageAPI
+    {
+		private readonly string _apiKey;
+		private readonly string _baseUrl = "https://www.alphavantage.co/query";
+
+		public AlphaVantageAPI(string apiKey)
+		{
+			_apiKey = apiKey;
+		}
+
+		public async Task<string> GetForexDataAsync(string fromSymbol, string toSymbol, string outputsize = "compact")
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				var url = $"{_baseUrl}?function=FX_DAILY&from_symbol={fromSymbol}&to_symbol={toSymbol}&outputsize={outputsize}&apikey={_apiKey}";
+
+				try
+				{
+					HttpResponseMessage response = await client.GetAsync(url);
+					response.EnsureSuccessStatusCode();
+
+					string responseData = await response.Content.ReadAsStringAsync();
+					return responseData; // Retorna os dados como string JSON
+				}
+				catch (Exception ex)
+				{
+					return $"Erro ao buscar os dados: {ex.Message}";
+				}
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -6,22 +6,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebFinancial.Controllers;
 
 namespace WebFinancial
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+		//ATENÇÃO: Lixo de Intalação do Visual Studio
+		//public static void Main(string[] args)
+		//{
+		//    CreateHostBuilder(args).Build().Run();
+		//}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-        //Comentario de teste
-    }
+		//public static IHostBuilder CreateHostBuilder(string[] args) =>
+		//    Host.CreateDefaultBuilder(args)
+		//        .ConfigureWebHostDefaults(webBuilder =>
+		//        {
+		//            webBuilder.UseStartup<Startup>();
+		//        });
+		//Comentario de teste
+
+		//CHAMADA API
+		
+		public static async Task Main(string[] args)
+		{
+			string apiKey = "NL4A7J42AZDAP4CG";
+			string fromSymbol = "USD";
+			string toSymbol = "BRL";
+
+			AlphaVantageAPI api = new AlphaVantageAPI(apiKey);
+			string data = await api.GetForexDataAsync(fromSymbol, toSymbol);
+
+			Console.WriteLine(data);
+		}
+	}
 }
